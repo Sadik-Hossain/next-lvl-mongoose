bee8eddb21d21a312841727c2d35818b08ed45ac
sadik <po480373@gmai.com>
2023-05-21 19:29:08 +0600

    createUserToDB hardcoded , getUserFromDb test done

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import { createUserToDB, getUserFromDB } from "./user.services";
 
 export const CreateUser = async (req: Request, res: Response) => {
   try {
-    res.status(200).json({ success: true });
+    const user = await createUserToDB();
+    res.status(200).json({ success: true, data: user });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
@@ -10,7 +12,8 @@ export const CreateUser = async (req: Request, res: Response) => {
 };
 export const getUser = async (req: Request, res: Response) => {
   try {
-    res.status(200).json({ success: true });
+    const user = await getUserFromDB();
+    res.status(200).json({ success: true, data: user });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
---
+++ src/modules/user/user.services.ts
@@ -0,0 +1,27 @@
+import { IUser } from "./user.interface";
+import User from "./user.model";
+
+export const getUserFromDB = async () => {
+  const result = await User.find();
+  return result;
+};
+
+export const createUserToDB = async (): Promise<IUser> => {
+  const newUser = new User({
+    id: "1",
+    role: "student",
+    password: "12",
+    name: {
+      firstName: "a",
+      middleName: "b",
+      lastName: "c",
+    },
+    gender: "male",
+    contactNo: "111",
+    emergencyContactNo: "111",
+    presentAddress: "zxXxX",
+    permanentAddress: "zxXxX",
+  });
+  const result = await newUser.save();
+  return result;
+};
